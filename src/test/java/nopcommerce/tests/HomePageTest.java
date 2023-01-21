7631f59e2c215bd3bc9bfc851daa7082372691b9
NabilAmrouche <nab.amrouche@gmail.com>
2023-01-21 15:29:20 -0500

    N first push

---
+++ src/test/java/nopcommerce/tests/HomePageTest.java
@@ -0,0 +1,25 @@
+package nopcommerce.tests;
+
+import base.CommonAPI;
+import nopcommerce.pages.HomePage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class HomePageTest extends CommonAPI {
+    Logger log = LogManager.getLogger(HomePage.class.getName());
+
+   @Test
+    public void clkOnLnkRegisterTest () throws InterruptedException {
+
+        HomePage homePage = new HomePage(getDriver());
+        Thread.sleep(3000);
+        homePage.clkOnLnkRegister();
+       String actualTitle = getCurrentTitle();
+        Assert.assertEquals(actualTitle,"nopCommerce demo store. Register");
+
+    }
+}
