81048c1c9b298eaeadcf8303b77ada06bb866d23
hassenehadjour98 <hassenehadjour98@gmail.com>
2023-02-04 18:45:26 -0500

    Final step

--- src/main/java/base/CommonAPI.java
+++ src/main/java/base/CommonAPI.java
@@ -37,12 +37,12 @@ import java.util.*;
 
 public class CommonAPI {
     Logger LOG = LogManager.getLogger(CommonAPI.class.getName());
+    String username = Utility.decode(Utility.getProperties().getProperty("browserstack.username").trim());
+    String password = Utility.decode(Utility.getProperties().getProperty("browserstack.password").trim());
 
     String takeScreenshot = Utility.getProperties().getProperty("take.screenshot", "false");
     String maximizeBrowser = Utility.getProperties().getProperty("browser.maximize", "true");
     String implicitWait = Utility.getProperties().getProperty("implicit.wait", "10");
-    String username = Utility.decode(Utility.getProperties().getProperty("browserstack.username").trim());
-    String password = Utility.decode(Utility.getProperties().getProperty("browserstack.password").trim());
     String headlessMode = Utility.getProperties().getProperty("headless.mode", "false");
 
     public WebDriver driver;
@@ -63,6 +63,7 @@ public class CommonAPI {
         ExtentTestManager.startTest(method.getName());
         ExtentTestManager.getTest().assignCategory(className);
     }
+
     protected String getStackTrace(Throwable t) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
@@ -88,32 +89,36 @@ public class CommonAPI {
         }
         ExtentTestManager.endTest();
         extent.flush();
-        if (takeScreenshot.equalsIgnoreCase("true")){
+        if (takeScreenshot.equalsIgnoreCase("true")) {
             if (result.getStatus() == ITestResult.FAILURE) {
                 takeScreenshot(result.getName());
             }
         }
         driver.quit();
     }
+
     @AfterSuite
     public void generateReport() {
         extent.close();
     }
+
     private Date getTime(long millis) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(millis);
         return calendar.getTime();
     }
-    public void getLocalDriver(String browserName){
+
+    public void getLocalDriver(String browserName) {
         ChromeOptions options = new ChromeOptions();
-        if (browserName.equalsIgnoreCase("chrome")){
+        if (browserName.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver(options.setHeadless(Boolean.parseBoolean(headlessMode)));
-        }else if (browserName.equalsIgnoreCase("firefox")) {
+        } else if (browserName.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
         } else if (browserName.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
         }
     }
+
     public void getCloudDriver(String envName, String os, String osVersion, String browser,
                                String browserVersion, String username, String password) throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
@@ -121,33 +126,35 @@ public class CommonAPI {
         cap.setCapability("os_version", osVersion);
         cap.setCapability("browser", browser);
         cap.setCapability("browser_version", browserVersion);
-        if (envName.equalsIgnoreCase("browserstack")){
+        if (envName.equalsIgnoreCase("browserstack")) {
             cap.setCapability("resolution", "1024x768");
-            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+
-                    "@hub-cloud.browserstack.com:80/wd/hub"),cap);
+            driver = new RemoteWebDriver(new URL("http://" + username + ":" + password +
+                    "@hub-cloud.browserstack.com:80/wd/hub"), cap);
         } else if (envName.equalsIgnoreCase("saucelabs")) {
-            driver = new RemoteWebDriver(new URL("http://"+username+":"+password+
-                    "@ondemand.saucelabs.com:80/wd.hub"),cap);
+            driver = new RemoteWebDriver(new URL("http://" + username + ":" + password +
+                    "@ondemand.saucelabs.com:80/wd.hub"), cap);
         }
 
     }
-    @Parameters({"useCloudEnv","envName","os","osVersion","browserName","browserVersion","url"})
+
+    @Parameters({"useCloudEnv", "envName", "os", "osVersion", "browserName", "browserVersion", "url"})
     @BeforeMethod()
     public void setUp(@Optional("false") boolean useCloudEnv, @Optional("browserstack") String envName,
                       @Optional("windows") String os, @Optional("11") String osVersion,
                       @Optional("chrome") String browserName, @Optional("108") String browserVersion,
                       @Optional("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") String url) throws InterruptedException, MalformedURLException {
-        if (useCloudEnv){
-            getCloudDriver(envName, os,osVersion,browserName,browserVersion, username, password);
-        }else {
+        if (useCloudEnv) {
+            getCloudDriver(envName, os, osVersion, browserName, browserVersion, username, password);
+        } else {
             getLocalDriver(browserName);
         }
-        if (maximizeBrowser.equalsIgnoreCase("true")){
+        if (maximizeBrowser.equalsIgnoreCase("true")) {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(Integer.parseInt(implicitWait)));
         }
         driver.manage().window().maximize();
         driver.get(url);
     }
+
     //    @AfterMethod
 //    public void tearDown(){
 //        driver.close();
@@ -156,113 +163,136 @@ public class CommonAPI {
     public WebDriver getDriver() {
         return driver;
     }
-    public String getCurrentTitle(){
+
+    public String getCurrentTitle() {
         return driver.getTitle();
     }
-    public String getTextFromElement(WebElement element){
+
+    public String getTextFromElement(WebElement element) {
         return element.getText();
     }
-    public void clickOn(WebElement element){
+
+    public void clickOn(WebElement element) {
         element.click();
     }
-    public void type(WebElement element, String text){
+
+    public void type(WebElement element, String text) {
         element.sendKeys(text);
     }
-    public void typeAndEnter(WebElement element, String text){
+
+    public void typeAndEnter(WebElement element, String text) {
         element.sendKeys(text, Keys.ENTER);
     }
-    public void selectOptionFromDropdown(WebElement dropdown, String option){
+
+    public void selectOptionFromDropdown(WebElement dropdown, String option) {
         Select select = new Select(dropdown);
         try {
             select.selectByVisibleText(option);
-        }catch (Exception e){
+        } catch (Exception e) {
             select.selectByValue(option);
         }
     }
-    public void hoverOver(WebDriver driver, WebElement element){
+
+    public void hoverOver(WebDriver driver, WebElement element) {
         Actions actions = new Actions(driver);
         actions.moveToElement(element).build().perform();
     }
-    public void waitForElementToBeVisible(WebDriver driver, int duration, WebElement element){
+
+    public void waitForElementToBeVisible(WebDriver driver, int duration, WebElement element) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(duration));
         wait.until(ExpectedConditions.visibilityOf(element));
 
     }
+
     public WebElement waitForElementToBeVisible(By locator, int timeOut, long intervalTime) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOut), Duration.ofMillis(intervalTime));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
-    public void clickWithActions(WebDriver driver, WebElement element){
+
+    public void clickWithActions(WebDriver driver, WebElement element) {
         Actions actions = new Actions(driver);
         actions.moveToElement(element).click().build().perform();
     }
-    public void clickWithJavascript(WebElement element){
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+    public void clickWithJavascript(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].click();", element);
     }
+
     public void captureScreenshot() {
-        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(file,new File("screenshots"+File.separator+"screenshot.png"));
+            FileUtils.copyFile(file, new File("screenshots" + File.separator + "screenshot.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void takeScreenshot(String screenshotName){
+
+    public void takeScreenshot(String screenshotName) {
         DateFormat df = new SimpleDateFormat("MMddyyyyHHmma");
         Date date = new Date();
         df.format(date);
 
-        File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         try {
-            FileUtils.copyFile(file, new File(Utility.path + File.separator +"screenshots"+ File.separator + screenshotName+" "+df.format(date)+".jpeg"));
+            FileUtils.copyFile(file, new File(Utility.path + File.separator + "screenshots" + File.separator + screenshotName + " " + df.format(date) + ".jpeg"));
             LOG.info("Screenshot captured");
         } catch (Exception e) {
-            LOG.info("Exception while taking screenshot "+e.getMessage());
+            LOG.info("Exception while taking screenshot " + e.getMessage());
         }
     }
+
     //Added Methods
-    public String getCurrentURL(){
-            return driver.getCurrentUrl();
-        }
-    public String getAttributeValue (WebElement element, String attributeName){
+    public String getCurrentURL() {
+        return driver.getCurrentUrl();
+    }
+
+    public String getAttributeValue(WebElement element, String attributeName) {
         return element.getAttribute(attributeName);
     }
-    public boolean elementIsDisplayed (WebElement element){
+
+    public boolean elementIsDisplayed(WebElement element) {
         return element.isDisplayed();
     }
-    public boolean elementIsSelected (WebElement element){
+
+    public boolean elementIsSelected(WebElement element) {
         return element.isSelected();
     }
-    public boolean elementIsEnabled (WebElement element){
+
+    public boolean elementIsEnabled(WebElement element) {
         return element.isEnabled();
     }
-    public void acceptAlert(){
+
+    public void acceptAlert() {
         driver.switchTo().alert().accept();
     }
-    public void dismissAlert(){
+
+    public void dismissAlert() {
         driver.switchTo().alert().dismiss();
     }
-    public String getTextFromAlert(){
+
+    public String getTextFromAlert() {
         return driver.switchTo().alert().getText();
     }
-    public void clearTextFromTextBox (WebElement element){
+
+    public void clearTextFromTextBox(WebElement element) {
         element.clear();
     }
-    public void typeNumber(WebElement element, int number){
-        element.sendKeys(""+number+"");
+
+    public void typeNumber(WebElement element, int number) {
+        element.sendKeys("" + number + "");
     }
-    public void switchToChildWindow(WebDriver driver, String Title){
+
+    public void switchToChildWindow(WebDriver driver, String Title) {
         Set<String> allWindowHandles = driver.getWindowHandles();
         List<String> hList = new ArrayList<String>(allWindowHandles);
-        for(String window : hList){
+        for (String window : hList) {
             String url = driver.switchTo().window(window).getCurrentUrl();
-            String windowHandle=driver.switchTo().window(window).getWindowHandle();
-            if(url.contains(Title)){
+            String windowHandle = driver.switchTo().window(window).getWindowHandle();
+            if (url.contains(Title)) {
                 driver.switchTo().window(windowHandle);
                 break;
             }
         }
     }
-
 }
